ec2a3e1f966038aa6d0b79b5bd576255bc30fd49
karishma-sureka <karishma2812@gmail.com>
2016-02-27 15:38:04 -0800

    Facial features, Emotion, Integrated bing search OCR. Bug in the Clarify auth.

---
+++ restHacktech.py
@@ -0,0 +1,136 @@
+import httplib, urllib, base64, urllib2, json
+
+def face_api_call(link):
+	print "\nFACE FEATURES API\n"
+	headers = {
+	'Content-Type': 'application/json',
+	'Ocp-Apim-Subscription-Key': '1255a5bd032544d39e72c04c7ba5a5bb',
+	}
+	params = urllib.urlencode({
+		'returnFaceId': 'true',
+		'returnFaceLandmarks': 'false',
+		'returnFaceAttributes': 'age,gender,smile',
+	})
+
+	try:
+		url = '{ "url" : "' + link + '"}'
+		conn = httplib.HTTPSConnection('api.projectoxford.ai')
+		conn.request("POST", "/face/v1.0/detect?%s" % params, url, headers)
+		response = conn.getresponse()
+		data = response.read()
+		print(data), "\n"
+		conn.close()
+	except Exception as e:
+		print("[Errno {0}] {1}".format(e.errno, e.strerror))
+
+
+def face_emotion_call(link):
+	print "\nFACE EMOTION API \n"
+	headers = {
+	'Content-Type': 'application/json',
+	'Ocp-Apim-Subscription-Key': '5d149c37b04a4d70b77490cfe027f868',
+	}
+	try:
+		url = '{ "url" : "' + link + '"}'
+		conn = httplib.HTTPSConnection('api.projectoxford.ai')
+		conn.request("POST", "/emotion/v1.0/recognize", url, headers)
+		response = conn.getresponse()
+		data = response.read()
+		print(data), "\n"
+		conn.close()
+	except Exception as e:
+			print("[Errno {0}] {1}".format(e.errno, e.strerror))
+
+def search_ocr_call(query):
+	print "\nQUERY SEARCH AND OCR API \n"
+	headers = {
+	'Content-Type': 'application/json',
+	'Ocp-Apim-Subscription-Key': '55f752e2761b4c8492c4f12e9f9cd0e1',
+	}
+	params = urllib.urlencode({
+		'language': 'unk',
+		'detectOrientation ': 'true',
+	})
+
+	search_type = "Image"
+	key= 'FNCeR+bi9PhI9JjDXKmpZ3rUkGWWJ3Zz7M72PQDCniQ'
+	query = urllib.quote(query)
+
+	credentials = (':%s' % key).encode('base64')[:-1]
+	auth = 'Basic %s' % credentials
+	url = 'https://api.datamarket.azure.com/Data.ashx/Bing/Search/'+search_type+'?Query=%27'+query+'%27&$top=10&$format=json'
+	request = urllib2.Request(url)
+	request.add_header('Authorization', auth)
+	request_opener = urllib2.build_opener()
+	response = request_opener.open(request) 
+	response_data = response.read()
+	json_result = json.loads(response_data)
+	#print json_result
+
+	for i in json_result['d']['results']:
+		url = '{ "url" : "' + str(i['MediaUrl'].encode('ascii', 'ignore')) + '"}'
+		try:
+			conn = httplib.HTTPSConnection('api.projectoxford.ai')
+			conn.request("POST", "/vision/v1/ocr?%s" % params, url, headers)
+			response = conn.getresponse()
+			data = response.read()
+			json_result = json.loads(data)
+			output = ''
+			for r in json_result['regions']:
+				for l in r['lines']:
+					for w in l['words']:
+						output += w['text']
+						output += " "
+			print output, "\n"
+			conn.close()
+		except Exception as e:
+			print("[Errno {0}] {1}".format(e.errno, e.strerror))
+
+def get_clarifai_tags(url):
+	print "\nCLARIFAI API\n"
+	method = "POST"
+	handler = urllib2.HTTPHandler()
+	opener = urllib2.build_opener(handler)
+	param = '{ "url" :"' + url + '"}'
+	data = urllib.urlencode(param)
+	request = urllib2.Request('https://api.clarifai.com/v1/tag', data)
+	request.add_header('Authorization', 'Bearer NbiYl9IAsjm75XqjS8gef0rZEeZTnS')
+	request.get_method = lambda: method
+	try:
+		connection = opener.open(request)
+	except urllib2.HTTPError,e:
+		connection = e
+
+	if connection.code == 200:
+		data = connection.read()
+		data = json.loads(data)
+		classList = data["results"][0]["result"]["tag"]["classes"]
+		print classList + "\n"
+
+def img_tag_category_call(link):
+	print "\nFEATURES - TAGS AND CATEGORIES \n"
+	headers = {
+	'Content-Type': 'application/json',
+	'Ocp-Apim-Subscription-Key': '55f752e2761b4c8492c4f12e9f9cd0e1',
+	}
+
+	params = urllib.urlencode({
+		'visualFeatures': 'All',
+	})
+	try:
+		url = '{ "url" : "' + link + '"}'
+		conn = httplib.HTTPSConnection('api.projectoxford.ai')
+		conn.request("POST", "/vision/v1/analyses?%s" % params, url, headers)
+		response = conn.getresponse()
+		data = response.read()
+		print(data), "\n"
+		conn.close()
+		get_clarifai_tags(link)
+	except Exception as e:
+		print("[Errno {0}] {1}".format(e.errno, e.strerror))
+
+link = "http://akonthego.com/blog/wp-content/uploads/2012/07/DSC_5129.jpg"
+face_api_call(link)
+face_emotion_call(link)
+search_ocr_call("Funny Calvin Hobbes quotes")
+img_tag_category_call(link)
